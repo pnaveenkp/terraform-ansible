2bf8aba456f9b18e5b89cfdfcd1b3d2fd77081a3
pnaveenkp <naveenkumar123243@gmail.com>
2023-05-06 23:35:44 +0530

    added ansible-vault

--- local_file_docker.tf
+++ local_file_docker.tf
@@ -1,14 +1,14 @@
-resource "local_file" "docker-inventory-file" {
-  content = templatefile("docker-details.tpl",
-    {
+# resource "local_file" "docker-inventory-file" {
+#   content = templatefile("docker-details.tpl",
+#     {
 
-      testserver01 = aws_instance.web-1.private_ip
-      testserver02 = aws_instance.web-2.private_ip
-      testserver03 = aws_instance.web-3.private_ip
-      testserver04 = aws_instance.web-4.private_ip
-      testserver05 = aws_instance.web-5.private_ip
-      testserver06 = aws_instance.web-6.private_ip
-    }
-  )
-  filename = "dockerinvfile"
-}
+#       testserver01 = aws_instance.web-1.private_ip
+#       testserver02 = aws_instance.web-2.private_ip
+#       testserver03 = aws_instance.web-3.private_ip
+#       testserver04 = aws_instance.web-4.private_ip
+#       testserver05 = aws_instance.web-5.private_ip
+#       testserver06 = aws_instance.web-6.private_ip
+#     }
+#   )
+#   filename = "dockerinvfile"
+# }
